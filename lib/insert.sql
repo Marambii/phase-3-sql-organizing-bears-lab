6f290b5402a93d328259c87b37ff659fa5d1a0a6
Baruch Marambi Mwenda <marambib2019@gmail.com>
2023-05-26 13:41:08 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, colour, temperament,alive )
+    VALUES ("Mr. Chocolate", 10, "M", "brown", "sassy", 1),
+    ("Rowdy", 10, "M", "brown", "Calm", 0),
+    ("Tabitha", 10, "F", "black", "shy", 1),
+    ("Seargent Brown", 10, "M", "white", "overbearing", 0),
+    ("Melissa", 10, "F", "brown", "calm", 1),
+    ("Grinch", 10, "M", "brown", "arrogant", 1),
+    ("Wendy", 10, "F", "black", "joker", 0),
+    ( Null, 10, "F", "white", "sassy", 1)
